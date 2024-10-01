9c8573849589937a5a81b82e02627b1288fbbb07
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-01 23:41:11 +0900

    #1 env: nextjs 및 필수 라이브러리 설치, eslint, prettier 설치
        "@tanstack/react-query": "^5.56.2",
        "axios": "^1.7.7",
        "classnames": "^2.5.1",
        "framer-motion": "^11.9.0",
        "next": "14.2.13",
        "react": "^18",
        "react-dom": "^18",
        "zustand": "^5.0.0-rc.2"
        "tailwindcss": "^3.4.1",
        "typescript": "^5"

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next';
+import '@/styles/globals.css';
+
+export const metadata: Metadata = {
+  title: '포켓몬 도감',
+  description: '세상의 모든 포켓몬이 여기에!',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
