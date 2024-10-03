1eddf46cf4d82386ebdf739ce10a325045e012e6
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-03 22:05:08 +0900

    #2 feat: 열리고 닫힐 때, 애니메이션을 표출하는 메뉴 컴포넌트 구현

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,42 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      keyframes: {
+        'scale-in-ver-center': {
+          '0%': {
+            transform: 'scaleY(0)',
+            opacity: '1',
+          },
+          '100%': {
+            transform: 'scaleY(1)',
+            opacity: '1',
+          },
+        },
+        'scale-out-ver-center': {
+          '0%': {
+            transform: 'scaleY(1)',
+            opacity: '1',
+          },
+          '100%': {
+            transform: 'scaleY(0)',
+            opacity: '1',
+          },
+        },
+      },
+      animation: {
+        scaleInVerCenter: 'scale-in-ver-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+        scaleOutVerCenter: 'scale-out-ver-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
