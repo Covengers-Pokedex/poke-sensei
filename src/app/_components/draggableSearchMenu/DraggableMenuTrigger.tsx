030d93686f6dc1d7f071741c860cfa0c23b9ef5a
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-04 23:52:28 +0900

    #2 fix: 포켓몬 이미지를 작은 gif 이미지로 변경하고 레이아웃 수정

--- src/app/_components/draggableSearchMenu/DraggableMenuTrigger.tsx
+++ src/app/_components/draggableSearchMenu/DraggableMenuTrigger.tsx
@@ -30,7 +30,7 @@ export default function DraggableMenuTrigger() {
         onDragStart={() => setIsDragging(true)}
         onDragEnd={() => setIsDragging(false)}
         whileDrag={{ cursor: 'grabbing' }}
-        className={'absolute'}
+        className={'absolute pointer-events-auto '}
         dragConstraints={constraintsRef} // 드래그 요소가 넘어가지 못하는 경계 설정
         dragTransition={{ bounceStiffness: 500, bounceDamping: 20 }} // 드래그 요소가 경계를 벗어날 때 튕겨서 돌아오는 효과
         dragElastic={0.5} // 경계 구간에 얼마나 들어갈 수 있는 지 설정
@@ -39,7 +39,7 @@ export default function DraggableMenuTrigger() {
         <div className={'flex gap-5'}>
           <article
             onClick={handleMenuDisplayClick}
-            className={classNames('pointer-events-auto hover:cursor-pointer', isDragging && 'hover:cursor-grab')}
+            className={classNames('hover:cursor-pointer', isDragging && 'hover:cursor-grab')}
           >
             <MonsterBall />
           </article>
