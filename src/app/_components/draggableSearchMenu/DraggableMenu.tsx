acbfa836bf4a8fab404acabcf75fa5d837eda7c3
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-04 23:26:08 +0900

    #2 fix: 기존 absolute 처리했던 DraggableMenu 영역을 fixed로 변경
    - 항상 도감 영역을 덮고 있어야 하므로 relative 영역이 아니라, 뷰포트를 기준으로 하는 fixed로 변경

--- src/app/_components/draggableSearchMenu/DraggableMenu.tsx
+++ src/app/_components/draggableSearchMenu/DraggableMenu.tsx
@@ -21,32 +21,30 @@ export default function DraggableMenu() {
   };
 
   return (
-    <div className={'relative'}>
-      <div ref={constraintsRef} className={'absolute h-[100dvh] w-[100dvw] -z-10'}>
-        <SearchMenuContainer isOpenMenu={isOpen} onCloseMenuClick={handleMenuDisplayClick} />
-        <motion.div
-          ref={menuContainer}
-          dragControls={dragControls}
-          drag
-          onDragStart={() => setIsDragging(true)}
-          onDragEnd={() => setIsDragging(false)}
-          whileDrag={{ cursor: 'grabbing' }}
-          className={'absolute'}
-          dragConstraints={constraintsRef} // 드래그 요소가 넘어가지 못하는 경계 설정
-          dragTransition={{ bounceStiffness: 500, bounceDamping: 20 }} // 드래그 요소가 경계를 벗어날 때 튕겨서 돌아오는 효과
-          dragElastic={0.5} // 경계 구간에 얼마나 들어갈 수 있는 지 설정
-          style={{ touchAction: 'none' }} // 모바일에서 터치 가능하려면 넣어야 하는 설정
-        >
-          <div className={'flex gap-5'}>
-            <article
-              onClick={handleMenuDisplayClick}
-              className={classNames('hover:cursor-pointer', isDragging && 'hover:cursor-grab')}
-            >
-              <MonsterBall />
-            </article>
-          </div>
-        </motion.div>
-      </div>
+    <div ref={constraintsRef} className={'fixed top-0 left-0 h-[100dvh] w-[100dvw] pointer-events-none'}>
+      <SearchMenuContainer isOpenMenu={isOpen} onCloseMenuClick={handleMenuDisplayClick} />
+      <motion.div
+        ref={menuContainer}
+        dragControls={dragControls}
+        drag
+        onDragStart={() => setIsDragging(true)}
+        onDragEnd={() => setIsDragging(false)}
+        whileDrag={{ cursor: 'grabbing' }}
+        className={'absolute'}
+        dragConstraints={constraintsRef} // 드래그 요소가 넘어가지 못하는 경계 설정
+        dragTransition={{ bounceStiffness: 500, bounceDamping: 20 }} // 드래그 요소가 경계를 벗어날 때 튕겨서 돌아오는 효과
+        dragElastic={0.5} // 경계 구간에 얼마나 들어갈 수 있는 지 설정
+        style={{ touchAction: 'none' }} // 모바일에서 터치 가능하려면 넣어야 하는 설정
+      >
+        <div className={'flex gap-5'}>
+          <article
+            onClick={handleMenuDisplayClick}
+            className={classNames('pointer-events-auto hover:cursor-pointer', isDragging && 'hover:cursor-grab')}
+          >
+            <MonsterBall />
+          </article>
+        </div>
+      </motion.div>
     </div>
   );
 }
