b0c932e21b45faa6cba879dfef87321272be366c
kimsayhi <kimse418@gmail.com>
2024-10-04 20:34:56 +0900

    #3 design: 포켓몬리스트 flex box를 grid로 수정, 이름 8자 까지 깨짐없음

--- src/app/_components/main/PokemonList.tsx
+++ src/app/_components/main/PokemonList.tsx
@@ -24,21 +24,21 @@ export default function PokemonList() {
     { id: 19, name: '이상해씨', types: ['풀', '독'], image: esanghe },
     { id: 20, name: '이상해씨', types: ['풀', '독'], image: esanghe },
   ];
-
+  ``;
   return (
-    <div className="flex gap-4  flex-wrap justify-around pt-[40%]">
+    <div className="grid gap-4 justify-items-center grid-cols-[repeat(auto-fit,minmax(210px,1fr))]  pt-[40%]">
       {mockData.map(pokemon => (
         <div
           key={pokemon.id}
-          className="flex transition-all ease-in duration-200 hover:scale-110 bg-white rounded-xl shadow-md flex-col border w-[210px] relative h-[210px] p-4 items-center justify-between"
+          className="flex transition-all ease-in duration-200 hover:scale-110 bg-white rounded-xl shadow-md flex-col border relative w-[210px] h-[210px]  p-4 items-center justify-between"
         >
-          <div className="flex justify-between items-center w-full">
-            <span className="pl-2 text-xs text-gray-500">No.{pokemon.id}</span>
-            <span>{pokemon.name}</span>
-            <button className="w-[30px] h-[30px] relative">
-              <Image className="opacity-50" src={monsterBall} alt="즐겨찾기" fill />
-            </button>
-          </div>
+          <span className="absolute top-1 text-xs opacity-30 ">No.{pokemon.id}</span>
+          <span className="flex pt-1.5 justify-center w-full">{pokemon.name}</span>
+
+          <button className="w-[30px] h-[30px] absolute top-[21px] right-2">
+            <Image className="opacity-50" src={monsterBall} alt="즐겨찾기" fill />
+          </button>
+
           <Image src={pokemon.image} alt="포켓몬 이미지" width={50} height={50} />
           <div className="flex w-full gap-2 flex-nowrap ">
             {pokemon.types.map(type => (
--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,7 +1,7 @@
 import PokemonList from '../_components/main/PokemonList';
 export default function MainPage() {
   return (
-    <div className="max-w-[1200px] m-auto mt-20 rounded-xl px-10 min-h-[100vh] bg-gray-200">
+    <div className="max-w-[1200px] m-10 xl:m-auto mt-20 rounded-xl px-5 xl:px-10 min-h-[100vh] bg-gray-200">
       <PokemonList />
     </div>
   );
