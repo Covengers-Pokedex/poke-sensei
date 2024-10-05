d42a50ec46cb49af2405939e15c5840cbc2e41ee
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-05 18:42:00 +0900

    #10 fix: 모달 프레임 테스트 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,3 @@
-'use client';
-
-import { useToggle } from '@/hooks/useToggle';
-import ModalFrame from './_components/modal/ModalFrame';
-
 export default function Landing() {
-  const { toggleValue, switchToggle, turnOffToggle } = useToggle();
-  return (
-    <div>
-      <button onClick={switchToggle}>test button</button>
-      <ModalFrame isOpenModal={toggleValue} closeModal={turnOffToggle}>
-        <div className="h-96 w-96 bg-slate-600">모달 내부 테스트</div>
-      </ModalFrame>
-      Landing
-    </div>
-  );
+  return <div>Landing</div>;
 }
