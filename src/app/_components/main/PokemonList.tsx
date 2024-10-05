819c10d5e7178ee2e0e658b5a2d1c33f2c9833b4
kimsayhi <kimse418@gmail.com>
2024-10-05 22:11:42 +0900

    #3 feat: 초기 포켓몬리스트 SSR 랜더링 및 더 불러오기 기능 구현

--- src/app/_components/main/PokemonList.tsx
+++ src/app/_components/main/PokemonList.tsx
@@ -1,54 +1,60 @@
+'use client';
 import Image from 'next/image';
-import esanghe from '@/images/pokemon/esanghe.jpg';
 import monsterBall from '@/images/items/poke-ball.webp';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { getPokemonAllList } from '@/lib/api/api';
+
 export default function PokemonList() {
-  const mockData = [
-    { id: 1, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 2, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 3, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 4, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 5, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 6, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 7, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 8, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 9, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 10, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 11, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 12, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 13, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 14, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 15, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 16, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 17, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 18, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 19, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-    { id: 20, name: '이상해씨', types: ['풀', '독'], image: esanghe },
-  ];
-  ``;
+  const {
+    data: pokemonData,
+    fetchNextPage,
+    error,
+  } = useInfiniteQuery({
+    queryKey: ['pokemon'],
+    queryFn: ({ pageParam }) => getPokemonAllList({ offset: pageParam, limit: 20 }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      if (lastPage.length >= 20) {
+        return allPages.length * 20;
+      }
+      return undefined;
+    },
+  });
+
   return (
-    <div className="grid gap-4 justify-items-center grid-cols-[repeat(auto-fit,minmax(210px,1fr))]  pt-[40%]">
-      {mockData.map(pokemon => (
-        <div
-          key={pokemon.id}
-          className="flex transition-all ease-in duration-200 hover:scale-110 bg-white rounded-xl shadow-md flex-col border relative w-[210px] h-[210px]  p-4 items-center justify-between"
-        >
-          <span className="absolute top-1 text-xs opacity-30 ">No.{pokemon.id}</span>
-          <span className="flex pt-1.5 justify-center w-full">{pokemon.name}</span>
+    <div className="grid gap-4 justify-items-center grid-cols-[repeat(auto-fit,minmax(210px,1fr))] pt-[40%]">
+      {pokemonData?.pages.map(pokemonList =>
+        pokemonList.map(pokemon => (
+          <div
+            key={pokemon.id}
+            className="flex transition-all ease-in duration-200 hover:scale-110 bg-white rounded-xl shadow-md flex-col border relative w-[210px] h-[210px] p-4 items-center justify-between"
+          >
+            <span className="absolute top-1 text-xs opacity-30">No.{pokemon.id}</span>
+            <span className="flex pt-1.5 justify-center w-full">{pokemon.name}</span>
 
-          <button className="w-[30px] h-[30px] absolute top-[21px] right-2">
-            <Image className="opacity-50" src={monsterBall} alt="즐겨찾기" fill />
-          </button>
+            <button className="w-[30px] h-[30px] absolute top-[21px] right-2">
+              <Image className="opacity-50" src={monsterBall} alt="즐겨찾기" fill />
+            </button>
 
-          <Image src={pokemon.image} alt="포켓몬 이미지" width={50} height={50} />
-          <div className="flex w-full gap-2 flex-nowrap ">
-            {pokemon.types.map(type => (
-              <span className="w-full border py-1 text-sm rounded-lg flex justify-center" key={type}>
-                {type}
-              </span>
-            ))}
+            <Image src={pokemon.image} alt="포켓몬 이미지" width={50} height={50} />
+
+            <div className="flex w-full gap-2 flex-nowrap">
+              {pokemon.typeList.map(type => (
+                <span className="w-full border py-1 text-sm rounded-lg flex justify-center" key={type.name}>
+                  {type.name}
+                </span>
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        )),
+      )}
+      <button
+        onClick={() => {
+          fetchNextPage();
+        }}
+      >
+        더보기
+      </button>
     </div>
   );
 }
--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,8 +1,25 @@
+import getQueryClient from '@/utils/getQueryClient';
 import PokemonList from '../_components/main/PokemonList';
-export default function MainPage() {
+import { getPokemonAllList } from '@/lib/api/api';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+export default async function MainPage() {
+  const queryClient = getQueryClient();
+
+  // 서버에서 데이터를 미리 가져옴
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: ['pokemon'],
+    queryFn: () => getPokemonAllList({ offset: undefined, limit: undefined }),
+    initialPageParam: 0,
+  });
+
+  const dehydratedState = dehydrate(queryClient);
   return (
-    <div className="max-w-[1200px] m-10 xl:m-auto mt-20 rounded-xl px-5 xl:px-10 min-h-[100vh] bg-gray-200">
-      <PokemonList />
-    </div>
+    <>
+      <HydrationBoundary state={dehydratedState}>
+        <div className="max-w-[1200px] m-10 xl:m-auto mt-20 rounded-xl px-5 xl:px-10 min-h-[100vh] bg-gray-200">
+          <PokemonList />
+        </div>
+      </HydrationBoundary>
+    </>
   );
 }
