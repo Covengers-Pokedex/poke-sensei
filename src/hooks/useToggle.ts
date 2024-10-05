492a929fea44e2ed9939f140780aec608ed6faf6
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-05 17:30:15 +0900

    #10 docs: useToggle 커스텀 훅 주석 추가

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -7,6 +7,8 @@ export const useToggle = () => {
     setToggle(prevState => !prevState);
   };
 
+  // 대부분의 경우 스위치 함수면 충분하겠지만,
+  // 외부 API 응답에 따라 토글을 특정 상태로 설정해야 하는 경우 등 반드시 열림과 닫힘을 구분해야 하는 경우에 사용한다.
   const turnOnToggle = () => {
     setToggle(true);
   };
