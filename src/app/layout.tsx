9d00f108254d190c0e8a71563791db9ca50e1205
yamdang <youm4000@naver.com>
2024-10-06 19:30:54 +0900

    #9 feat: 프리패치로 포켓몬 정보 미리 가져오기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,26 @@
+'use client';
+
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-export const metadata: Metadata = {
-  title: '포켓몬 도감',
-  description: '세상의 모든 포켓몬이 여기에!',
-};
+// export const metadata: Metadata = {
+//   title: '포켓몬 도감',
+//   description: '세상의 모든 포켓몬이 여기에!',
+// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <html lang="ko">
-      <body className="bg-[#A8D8A8]">{children}</body>
+      <body className="bg-[#A8D8A8]">
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,73 +1,42 @@
-'use client';
+import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
 import DraggableMenuTrigger from './_components/draggableSearchMenu/DraggableMenuTrigger';
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
+import PokemonQuiz from './_components/landing/PokemonQuiz';
+import getQueryClient from '@/lib/api/getQueryClient';
 import { getPokemonRandomImage } from '@/lib/api/api';
+import Link from 'next/link';
 
-interface Pokemon {
-  name: string | null;
-  image: string;
-  hint: string | null;
-}
+export default async function Landing() {
+  const queryClient = getQueryClient();
 
-export default function Landing() {
-  const FILTER_ON = 'filter brightness-0';
-  const FILTER_OUT = 'filter brightness-100';
-  const [randomPokemon, setRandomPokemon] = useState<Pokemon>({ name: '', image: '', hint: '' });
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [isError, setIsError] = useState<boolean>(false);
+  await queryClient.prefetchQuery({
+    queryKey: ['randomPokemon'],
+    queryFn: async () => {
+      const { pokemonName, pokemonRandomImage, pokemonHint } = await getPokemonRandomImage();
+      return { pokemonName, pokemonRandomImage, pokemonHint };
+    },
+    staleTime: 0,
+  });
 
-  useEffect(() => {
-    setIsLoading(true);
-    const fetchRandomPokemon = async () => {
-      try {
-        const { pokemonName, pokemonRandomImage, pokemonHint } = await getPokemonRandomImage();
-        console.log(pokemonName, pokemonRandomImage, pokemonHint);
-        setIsLoading(false);
-        setRandomPokemon({ name: pokemonName, image: pokemonRandomImage, hint: pokemonHint });
-      } catch (error) {
-        setIsError(true);
-        console.error(error);
-      }
-    };
-    fetchRandomPokemon();
-  }, []);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isError) {
-    return <div>Error fetching Pokémon data</div>;
-  }
+  const dehydratedState = dehydrate(queryClient);
   return (
     <div>
       <DraggableMenuTrigger />
-      <div className="flex justify-center items-center absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-full min-h-[500px] pt-14 pb-10 max-w-[1200px] rounded-3xl bg-[#F2F4F6] border-4 border-[#ffffff]">
-        <div className="absolute w-full top-[-30px]">
-          <h1 className="title-line text-6xl text-center text-[#F9DC42]">오늘의 포켓몬은 뭘까요?</h1>
+      <div className="flex flex-col justify-between items-center gap-8 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-full min-h-[470px] sm:min-h-[500px] pb-6 sm:pb-10 max-w-[1200px] rounded-3xl bg-[#F2F4F6] border-4 border-[#ffffff] px-[10px]">
+        <div className="w-full mt-[-20px] sm:mt-[-30px]">
+          <h1 className="title-line text-2xl sm:text-5xl lg:text-6xl text-center text-[#F9DC42]">
+            오늘의 포켓몬은 뭘까요?
+          </h1>
         </div>
-        <div className="flex flex-col justify-center items-center">
-          <Image
-            className={`${FILTER_ON} mb-5`}
-            src={
-              isLoading && !randomPokemon.image
-                ? 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/ultra-ball.png'
-                : randomPokemon.image
-            }
-            width={250}
-            height={250}
-            alt="포켓몬 이미지"
-          />
-          <div className="flex flex-col justify-center items-center gap-3">
-            <p className="text-l">{isLoading ? '포켓몬을 데려오고 있어요!' : randomPokemon.hint}</p>
-            <input
-              type="text"
-              placeholder="포켓몬을 맞춰보세요!"
-              className="w-80 h-10 p-3 border-2 border-[#C5C5C5] rounded-lg"
-            />
-          </div>
-        </div>
+
+        <Link
+          href="/main"
+          className="absolute top-12 sm:top-10 text-sm sm:text-[16px] right-3 translate-y-[-50%] px-3 rounded-lg bg-[#D9D9D9] shadow-[2px_4px_4px_rgba(0,0,0,0.2)] transition-all hover:bg-[#F9DC42] leading-[30px] h-[30px] sm:leading-[35px] sm:h-[35px]"
+        >
+          포켓몬 도감
+        </Link>
+        <HydrationBoundary state={dehydratedState}>
+          <PokemonQuiz />
+        </HydrationBoundary>
       </div>
     </div>
   );
