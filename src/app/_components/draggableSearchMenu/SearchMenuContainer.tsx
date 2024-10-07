0d3a8474d415b0a2341f6e6da657d75efcc74292
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-07 21:47:16 +0900

    #16 feat: 포켓몬 검색 메뉴 뒤에 백드롭 컴포넌트 추가

--- src/app/_components/draggableSearchMenu/SearchMenuContainer.tsx
+++ src/app/_components/draggableSearchMenu/SearchMenuContainer.tsx
@@ -3,6 +3,7 @@ import SearchMenuContent from './SearchMenuContent';
 import Image from 'next/image';
 import defaultPokemonImage from '@/images/pokemon/pikachu.gif';
 import { useHidden } from '@/hooks/useHidden';
+import BackDrop from '../modal/BackDrop';
 
 interface SearchMenuContainerProps {
   isOpenMenu: boolean;
@@ -13,20 +14,27 @@ export default function SearchMenuContainer({ isOpenMenu, onCloseMenuClick }: Se
   const { isHidden } = useHidden(isOpenMenu);
 
   return (
-    <div className="absolute left-1/2 top-1/4 -translate-x-1/2 w-full flex justify-center pointer-events-auto">
-      <div
-        className={classNames(
-          'flex gap-4 flex-col items-center',
-          isOpenMenu ? 'visible animate-fadeInBottom' : 'animate-fadeOutBottom',
-          isHidden && 'hidden',
-        )}
-      >
-        <button onClick={onCloseMenuClick} className="absolute right-1 top-0">
-          x
-        </button>
-        <Image src={defaultPokemonImage} alt="포켓몬 이미지" height={75} width={75} unoptimized priority />
-        <SearchMenuContent />
-      </div>
-    </div>
+    <>
+      {!isHidden && (
+        <div className="pointer-events-auto">
+          <BackDrop closeModal={onCloseMenuClick} backdropBgColor="#000000" />
+          <div className="absolute left-1/2 top-1/4 -translate-x-1/2 modal-z-index">
+            <div
+              className={classNames(
+                'flex gap-4 flex-col items-center',
+                isOpenMenu ? 'animate-fadeInBottom' : 'animate-fadeOutBottom',
+                isHidden && 'hidden',
+              )}
+            >
+              <button onClick={onCloseMenuClick} className="absolute right-1 top-0 font-black">
+                x
+              </button>
+              <Image src={defaultPokemonImage} alt="포켓몬 이미지" height={75} width={75} unoptimized priority />
+              <SearchMenuContent />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
--- src/app/_components/modal/BackDrop.tsx
+++ src/app/_components/modal/BackDrop.tsx
@@ -1,5 +1,3 @@
-import classNames from 'classnames';
-
 interface BackDropProps {
   closeModal: () => void;
   backdropBgColor?: string;
@@ -9,7 +7,8 @@ export default function BackDrop({ closeModal, backdropBgColor }: BackDropProps)
   return (
     <div
       onClick={closeModal}
-      className={classNames('backdrop opacity-30 modal-backdrop-z-index', backdropBgColor && `bg-[${backdropBgColor}]`)}
+      className="backdrop opacity-30 modal-backdrop-z-index"
+      style={{ backgroundColor: backdropBgColor }}
     />
   );
 }
