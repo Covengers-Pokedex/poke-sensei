7f45cc65ed527cb1c662584855001f23ceb601fb
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-13 17:02:57 +0900

    #15 refector: 랜덤 포켓몬 gif 이미지 unoptimized 처리

--- src/app/_components/loading/RandomPokemon.tsx
+++ src/app/_components/loading/RandomPokemon.tsx
@@ -8,7 +8,14 @@ export default function RandomPokemon({ pokemonImgSrc }: { pokemonImgSrc: any })
       {/* 이미지가 없을 때 레이아웃 시프트를 없애기 위함 */}
       <div className="h-[80px]">
         {pokemonImgSrc && (
-          <Image className="h-[80px] w-auto" src={pokemonImgSrc} alt="로딩중 포켓몬 이미지" height={80} width={80} />
+          <Image
+            className="h-[80px] w-auto"
+            src={pokemonImgSrc}
+            alt="로딩중 포켓몬 이미지"
+            height={80}
+            width={80}
+            unoptimized
+          />
         )}
       </div>
       <p className="text-xl font-black">loading...</p>
