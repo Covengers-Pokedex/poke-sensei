07e2fbf05078f2f41aaa99f1860efa6a2f3c5c8a
kimsayhi <kimse418@gmail.com>
2024-11-13 20:27:01 +0900

    fix: 삭제된 파일 다시 머지실수로 다시 생성된 오류 수정

--- src/app/_components/DraggableMenu/SearchMenu.tsx
+++
@@ -1,9 +0,0 @@
-import classNames from 'classnames';
-
-interface SearchMenuProps {
-  isOpenMenu: boolean;
-}
-
-export default function SearchMenu({ isOpenMenu }: SearchMenuProps) {
-  return <div className={classNames(isOpenMenu ? 'visible' : 'hidden')}>Menu</div>;
-}
