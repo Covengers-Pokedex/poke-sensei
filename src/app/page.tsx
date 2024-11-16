43aa2c95a95337624a72be660c64142569348c74
yamdang <youm4000@naver.com>
2024-11-16 19:19:49 +0900

    refactor: 랜딩페이지 여백 추가후 포켓몬 도감 버튼 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query
 import PokemonQuiz from './_components/landing/PokemonQuiz';
 import Link from 'next/link';
 import { getLoadingPokemonImage } from '@/lib/api/api';
+import pokemonDex from '@/images/items/pokemon-dex.png';
+import Image from 'next/image';
 
 export default async function Landing() {
   const queryClient = new QueryClient();
@@ -9,8 +11,8 @@ export default async function Landing() {
   await queryClient.prefetchQuery({ queryKey: ['loading'], queryFn: getLoadingPokemonImage });
 
   return (
-    <div>
-      <div className="flex flex-col justify-between items-center gap-8 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-full min-h-[500px] sm:min-h-[700px] pb-6 sm:pb-10 max-w-[1200px] rounded-3xl bg-[#F2F4F6] border-4 border-[#ffffff] px-[10px]">
+    <div className="flex justify-center items-center w-screen h-screen px-3 sm:px-5">
+      <div className="relative flex flex-col justify-between items-center gap-8 w-full min-h-[500px] sm:min-h-[700px] pb-6 sm:pb-10 max-w-[1200px] rounded-3xl bg-[#F2F4F6] border-4 border-[#ffffff] px-[10px]">
         <div className="w-full mt-[-20px] sm:mt-[-30px]">
           <h1 className="title-line text-2xl sm:text-5xl lg:text-6xl text-center text-[#F9DC42]">
             오늘의 포켓몬은 뭘까요?
@@ -19,8 +21,9 @@ export default async function Landing() {
 
         <Link
           href="/main"
-          className="absolute top-12 sm:top-10 text-sm sm:text-[16px] right-3 translate-y-[-50%] px-3 rounded-lg bg-[#D9D9D9] shadow-[2px_4px_4px_rgba(0,0,0,0.2)] transition-all hover:bg-[#F9DC42] leading-[30px] h-[30px] sm:leading-[35px] sm:h-[35px]"
+          className="flex items-center absolute top-11 sm:top-12 text-sm sm:text-[16px] right-3 translate-y-[-50%] px-3 rounded-lg bg-[#d0e8d0] shadow-[3px_5px_4px_rgba(0,0,0,0.15)] transition-all hover:bg-[#A8D8A8] leading-[35px] h-[35px] sm:leading-[40px] sm:h-[40px]"
         >
+          <Image src={pokemonDex} className="mr-2" width={20} height={20} alt="도감 이미지" />
           포켓몬 도감
         </Link>
         <HydrationBoundary state={dehydrate(queryClient)}>
