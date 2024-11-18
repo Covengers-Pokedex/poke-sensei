0dc29d985b0bbb52e8ef3441187974f8a5df8ead
yamdang <youm4000@naver.com>
2024-11-19 04:32:48 +0900

    feat: 로컬 스토리지에 저장된 포켓몬 id로 즐겨찾기 연동

--- src/app/_components/pokemonModal/components/FavoritePokemon.tsx
+++ src/app/_components/pokemonModal/components/FavoritePokemon.tsx
@@ -1,16 +1,27 @@
-import pokemonImage from '@/images/pokemon/pikachu.gif';
-import monsterBall from '@/images/items/poke-ball.webp';
 import Image from 'next/image';
+import PokePicker from '../../main/PokePicker';
 
-export default function FavoritePokemon() {
+export default function FavoritePokemon({
+  name,
+  image,
+  id,
+  onHandleRefreshData,
+}: {
+  name: string;
+  image: string;
+  id: number;
+  onHandleRefreshData: (id: number) => void;
+}) {
   return (
     <div className="flex flex-col items-center">
-      <Image src={pokemonImage} width={80} height={80} alt="포켓몬 이미지" />
+      <div className="w-[110px] h-[110px] sm:w-[120px] sm:h-[120px] relative">
+        <Image src={image} className="object-contain py-5" fill sizes="100vw, 100vw" alt="포켓몬 이미지" />
+      </div>
       <div className="flex items-center mt-2">
-        <h3 className="outline-text text-sm sm:text-base md:text-lg">피카츄</h3>
-        <button>
-          <Image className="opacity-100" width={30} height={30} src={monsterBall} alt="즐겨찾기" />
-        </button>
+        <h3 className="outline-text text-sm sm:text-base md:text-lg">{name}</h3>
+        <div className="flex items-center favorite-modal-important" onClick={() => onHandleRefreshData(Number(id))}>
+          <PokePicker id={id} name={name} />
+        </div>
       </div>
     </div>
   );
